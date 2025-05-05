f56bf735c3f8ccee73c366f9c6bdd47435bd0109
nguyen van hau <nguyenvanhau.soft@gmail.com>
2025-05-05 15:45:56 +0700

    add page login and register

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,21 +1,14 @@
-"use client";
 import { ModeToggle } from "@repo/ui/components/mode-toggle";
 import { Button } from "@repo/ui/components/ui/button";
 import { useTranslations } from "next-intl";
 import LocaleSwitcher from "~/components/LocaleSwitcher";
-import { api } from "~/trpc/react";
 
 export default function Home() {
   const t = useTranslations("HomePage");
-  let { data ,refetch} = api.hello.getMessageHello.useQuery();
-  const onBtnClick = () => {
-    refetch();
-    console.log(data);
-    console.log("Hello");
-  };
+
   return (
     <div>
-      <Button onClick={onBtnClick}>Hello</Button>
+      <Button>Hello</Button>
       <LocaleSwitcher />
       <h1 className="text-2xl font-bold">{t("demo")}</h1>
       <ModeToggle />
